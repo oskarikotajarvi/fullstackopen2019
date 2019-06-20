56941398ad6e215aa8fe672d95761cf19fa70148
oskarikotajarvi <oskari.kotajarvi@gmail.com>
2019-06-20 21:24:48 +0300

    Osa3 A,b

---
+++ osa3/puhelinluettelo/build/precache-manifest.2a2d453f638e86b3cf997a6667b0d6ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e45c2d482cca9c008a1041fa32c711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d5b0bd7847a2f5a7872",
+    "url": "/static/css/main.c8023828.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "3d5b0bd7847a2f5a7872",
+    "url": "/static/js/main.fdd64f77.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
